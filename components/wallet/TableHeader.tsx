7dcc49e1651632a3439f16817928a5643e98b706
Folakemi Orekoya <orekoyafolakemi@gmail.com>
2023-12-06 13:55:45 +0100

    update changes made to table header

--- components/wallet/TableHeader.tsx
+++ components/wallet/TableHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function TableHeader() {
   return (
-    <div className="bg-[#161F2C] font-bold leading-tight w-full">
+    <div className="bg-[#161F2C] font-bold leading-tight w-[100%]">
       <div className="flex items-center justify-between py-3 md:px-4 lg:px-4 border-none w-[100%]">
         <div className="w-2/4 md:w-1/4 lg:w-1/4  ml-8">
           <p className="text-[10px] md:text-base lg:text-base">Wallets</p>
